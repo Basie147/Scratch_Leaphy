a7f6885c1b782398c383a49a9b757c8819c87799
Basie147 <basdegraaf43@hotmail.com>
2024-06-06 17:04:10 +0200

    5 working leaphys

--- scratch_leaphy/Leaphy/Leaphy.js
+++ scratch_leaphy/Leaphy/Leaphy.js
@@ -6,23 +6,23 @@ import { csvFileData } from '../Lib/csv.js';
 
 // Define the constant
 const LEAPHYNUMBER = '1';
-
-
 let COMMAND_NUMBER = '996190084';
+
+
 document.getElementById("node-id_" + LEAPHYNUMBER).addEventListener('click', function() {
-  COMMAND_NUMBER = document.getElementById("Leaphy-id_1").value;});
+COMMAND_NUMBER = document.getElementById(`Leaphy-id_${LEAPHYNUMBER}`).value;});
 
-
-const xpos = 220 - LEAPHYNUMBER * 40;
-const ypos = -150;
-
+import { data } from '../Stage/Stage.js';
+const xpos =  data[58-LEAPHYNUMBER][0]; //- left + right
+const ypos = data[58- LEAPHYNUMBER][1] // + up - down
+const dir = data[58- LEAPHYNUMBER][2] // + up - down
 
 export default class Leaphy extends Sprite {
   constructor(...args) {
     super(...args);
-    this.costumes = [new Costume("Leaphy", "./costumes/Leaphy.png", { x: 273, y: 258 }) ];
+    this.costumes = [new Costume(`Leaphy`, `./costumes/Leaphy_${LEAPHYNUMBER}.png`, { x: 273, y: 258 })];
     this.triggers = [new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked)];
-    this.speed = 2.0; // Define speed as a class property
+    this.speed = 2; // Define speed as a class property
     this.turnDegree = 10; // Define turnDegree as a class property
     this.button = false; // initialize as a class property
     this.frame = 1/30; // Define frame as a class property
@@ -30,11 +30,12 @@ export default class Leaphy extends Sprite {
 
   // Interpret serial commands
   interpretSerialCommands = (text) => {
-    if (text.includes(COMMAND_NUMBER )) {
-      if (text.includes('Forward')) this.move(this.speed);
-      if (text.includes('stop')) this.move(0);
-      if (text.includes('Right')) this.direction -= this.turnDegree;
-      if (text.includes('Left')) this.direction += this.turnDegree;
+  const values = text.split(',');
+    if (values.length >= 2) {
+      const en_Left = parseFloat(values[0]);
+      this.goto(data[en_Left][0],data[en_Left][1]);
+      this.direction = data[en_Left][2];
+      console.log(en_Left);
     }
   };
 
@@ -45,7 +46,7 @@ export default class Leaphy extends Sprite {
     document.getElementById("myButton").addEventListener("click", () => { this.button = !this.button; });
   
     this.goto(xpos, ypos);
-    this.direction = 90;
+    this.direction = dir;
   
     document.getElementById("speedButton").addEventListener("click", () => {
       this.speed = parseFloat(document.getElementById("speedInput").value);
@@ -56,7 +57,7 @@ export default class Leaphy extends Sprite {
       this.turnDegree = parseInt(document.getElementById("turnInput").value);
       document.getElementById("turnValue").textContent = this.turnDegree;
     });
-   
+    
     document.getElementById("framerateButton").addEventListener("click", () => {
       this.frame = parseInt(document.getElementById("framerate").value);
       this.frame = 1 / this.frame;
@@ -64,7 +65,9 @@ export default class Leaphy extends Sprite {
     document.getElementById("templateButton").addEventListener("click", () => {
       const template = document.getElementById("template").value;
       if (template === "2") {
-      this.costumes = [new Costume("Leaphy", "./costumes/forklift.png", { x: 273, y: 258 })];
+        this.costumes = [new Costume("Leaphy", "./costumes/forklift.png" )];
+        this.goto(120, -140);
+        this.direction = 90;
       }
       else if (template === "5") {
         this.costumes = [new Costume("Leaphy", "./costumes/Bus.png", { x: 273, y: 258 })];
@@ -83,19 +86,19 @@ export default class Leaphy extends Sprite {
         const lightGreen = Color.rgb(122, 182, 73);
 
         if (this.colorTouching(yellow, red)) { // Yellow touching red
-     
+      
           csvFileData.push([`Leaphy_${LEAPHYNUMBER} is touching red`, "Yellow"]);
           yield* this.wait(5);
           for (let i = 0; i < 20; i++) {this.move(2); yield;}
         }
         if (this.colorTouching(yellow, green)) { // Yellow touching green
-         
+          
           csvFileData.push([`Leaphy_${LEAPHYNUMBER} is touching green`, "Yellow"]);
           yield* this.wait(3);
           for (let i = 0; i < 20; i++) {this.move(2); yield;}
         }
         if (this.colorTouching(yellow, blue)) { // Yellow touching blue
-         
+          
           csvFileData.push([`Leaphy_${LEAPHYNUMBER} is touching blue`, "Yellow"]);
           yield* this.wait(2);
           for (let i = 0; i < 20; i++) {this.move(2); yield;}
--- scratch_leaphy/Leaphy3/Leaphy3.js
+++ scratch_leaphy/Leaphy3/Leaphy3.js
@@ -1,23 +1,26 @@
+
 import { Sprite, Trigger, Costume, Color } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
 import { connectToSerial, registerListener } from '../Lib/serialFunctions.js';
 import { csvFileData } from '../Lib/csv.js';
 
 // Define the constant
 const LEAPHYNUMBER = '3';
-
 let COMMAND_NUMBER = '995460516';
+
 document.getElementById("node-id_" + LEAPHYNUMBER).addEventListener('click', function() {
-  COMMAND_NUMBER = document.getElementById("Leaphy-id_1").value;});
+COMMAND_NUMBER = document.getElementById(`Leaphy-id_${LEAPHYNUMBER}`).value;});
 
-const xpos = 220 - LEAPHYNUMBER * 40;
-const ypos = -150;
+import { data } from '../Stage/Stage.js';
+const xpos =  data[58-LEAPHYNUMBER][0]; //- left + right
+const ypos = data[58- LEAPHYNUMBER][1] // + up - down
+const dir = data[58- LEAPHYNUMBER][2] // + up - down
 
-export default class Leaphy3 extends Sprite {
+export default class Leaphy extends Sprite {
   constructor(...args) {
     super(...args);
-    this.costumes = [new Costume("Leaphy", "./costumes/Leaphy.png", { x: 273, y: 258 }) ];
+    this.costumes = [new Costume(`Leaphy`, `./costumes/Leaphy_${LEAPHYNUMBER}.png`, { x: 273, y: 258 })];
     this.triggers = [new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked)];
-    this.speed = 2.0; // Define speed as a class property
+    this.speed = 2; // Define speed as a class property
     this.turnDegree = 10; // Define turnDegree as a class property
     this.button = false; // initialize as a class property
     this.frame = 1/30; // Define frame as a class property
@@ -25,11 +28,12 @@ export default class Leaphy3 extends Sprite {
 
   // Interpret serial commands
   interpretSerialCommands = (text) => {
-    if (text.includes(COMMAND_NUMBER)) {
-      if (text.includes('Forward')) this.move(this.speed);
-      if (text.includes('stop')) this.move(0);
-      if (text.includes('Right')) this.direction -= this.turnDegree;
-      if (text.includes('Left')) this.direction += this.turnDegree;
+  const values = text.split(',');
+    if (values.length >= 2) {
+      const en_Left = parseFloat(values[0]);
+      this.goto(data[en_Left][0],data[en_Left][1]);
+      this.direction = data[en_Left][2];
+      console.log(en_Left);
     }
   };
 
@@ -40,7 +44,7 @@ export default class Leaphy3 extends Sprite {
     document.getElementById("myButton").addEventListener("click", () => { this.button = !this.button; });
   
     this.goto(xpos, ypos);
-    this.direction = 90;
+    this.direction = dir;
   
     document.getElementById("speedButton").addEventListener("click", () => {
       this.speed = parseFloat(document.getElementById("speedInput").value);
@@ -51,7 +55,7 @@ export default class Leaphy3 extends Sprite {
       this.turnDegree = parseInt(document.getElementById("turnInput").value);
       document.getElementById("turnValue").textContent = this.turnDegree;
     });
-   
+    
     document.getElementById("framerateButton").addEventListener("click", () => {
       this.frame = parseInt(document.getElementById("framerate").value);
       this.frame = 1 / this.frame;
@@ -59,7 +63,9 @@ export default class Leaphy3 extends Sprite {
     document.getElementById("templateButton").addEventListener("click", () => {
       const template = document.getElementById("template").value;
       if (template === "2") {
-      this.costumes = [new Costume("Leaphy", "./costumes/forklift.png", { x: 273, y: 258 })];
+        this.costumes = [new Costume("Leaphy", "./costumes/forklift.png" )];
+        this.goto(80, -140);
+        this.direction = 90;
       }
       else if (template === "5") {
         this.costumes = [new Costume("Leaphy", "./costumes/Bus.png", { x: 273, y: 258 })];
@@ -78,19 +84,19 @@ export default class Leaphy3 extends Sprite {
         const lightGreen = Color.rgb(122, 182, 73);
 
         if (this.colorTouching(yellow, red)) { // Yellow touching red
-     
+      
           csvFileData.push([`Leaphy_${LEAPHYNUMBER} is touching red`, "Yellow"]);
           yield* this.wait(5);
           for (let i = 0; i < 20; i++) {this.move(2); yield;}
         }
         if (this.colorTouching(yellow, green)) { // Yellow touching green
-         
+          
           csvFileData.push([`Leaphy_${LEAPHYNUMBER} is touching green`, "Yellow"]);
           yield* this.wait(3);
           for (let i = 0; i < 20; i++) {this.move(2); yield;}
         }
         if (this.colorTouching(yellow, blue)) { // Yellow touching blue
-         
+          
           csvFileData.push([`Leaphy_${LEAPHYNUMBER} is touching blue`, "Yellow"]);
           yield* this.wait(2);
           for (let i = 0; i < 20; i++) {this.move(2); yield;}
